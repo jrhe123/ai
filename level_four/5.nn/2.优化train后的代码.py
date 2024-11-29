b1d2e37607b184280dbfaea7bdf1e5c34bad340e
jrhe123 <keith_he@outlook.com>
2024-11-29 11:06:59 -0500

    update train func

--- level_four/5.nn/2.优化train后的代码.py
+++ level_four/5.nn/2.优化train后的代码.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 # 1. 加载数据集
 train_data = datasets.FashionMNIST(
@@ -68,18 +70,45 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 
+"""
+pred = torch.tensor([[0.1, 0.3, 0.6],
+                     [0.8, 0.1, 0.1]])
+print(pred.argmax(1))  # 输出: tensor([2, 0]) 表示第一个样本预测类别 2，第二个样本预测类别 0
+
+
+
+y = torch.tensor([2, 1])  # 真实标签
+pred_classes = torch.tensor([2, 0])  # 预测类别
+print(pred_classes == y)  # 输出: tensor([True, False])
+
+
+
+bool_tensor = torch.tensor([True, False])
+float_tensor = bool_tensor.type(torch.float)
+print(float_tensor)  # 输出: tensor([1.0, 0.0])
+"""
+
 # 6. 训练
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
+    num_batches = len(dataloader)
 
     # 调到训练模式
     model.train()
-    for batch, (X, y) in enumerate(dataloader):
+    loss_total = 0
+    correct = 0
+    for X, y in tqdm(dataloader):
         X, y = X.to(device), y.to(device)
 
         # Compute prediction and loss
         pred = model(X)
+
+        # 计算正确预测的数量
+        # 在 pred 张量中，沿着第 1 个维度（行方向）找到最大值所在的索引
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
         loss = loss_fn(pred, y)
+        loss_total += loss.item()
 
         # 反向传播
         loss.backward()
@@ -88,10 +117,10 @@ def train(dataloader, model, loss_fn, optimizer):
         # 优化器归零
         optimizer.zero_grad()
 
-        if batch % 100 == 0:
-            # 从tensor中取出数值 .item -> numpy格式
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    loss_avg = loss_total / num_batches
+    correct /= size
+
+    return round(correct, 3), round(loss_avg, 3)
 
 
 # 7. 测试
@@ -121,13 +150,32 @@ def test(dataloader, model, loss_fn):
 
 
 # 8. 训练和测试
-# epochs = 5
-# for t in range(epochs):
-#     print(f"Epoch {t + 1}\n-------------------------------")
-#     train(train_dataloader, model, loss_fn, optimizer)
-#     test(test_dataloader, model, loss_fn)
+train_acc_list = []
+train_loss_list = []
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train_acc, train_loss = train(train_dataloader, model, loss_fn, optimizer)
+    train_acc_list.append(train_acc)
+    train_loss_list.append(train_loss)
+    test(test_dataloader, model, loss_fn)
 
-# print("Done!")
+print("Done!")
+
+x_list = [i for i in range(len(train_acc_list))]
+plt.plot(x_list, train_acc_list, label="Train")
+plt.title("Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
+
+plt.plot(x_list, train_loss_list, label="Train")
+plt.title("Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
 
 
 # 9. 保存模型
@@ -135,27 +183,27 @@ def test(dataloader, model, loss_fn):
 # print("Trained model saved at model.pth")
 
 
-# 10. 加载模型
-model = NeuralNetwork().to(device)
-model.load_state_dict(torch.load("model.pth"))
-classes = [
-    "T-shirt/top",
-    "Trouser",
-    "Pullover",
-    "Dress",
-    "Coat",
-    "Sandal",
-    "Shirt",
-    "Sneaker",
-    "Bag",
-    "Ankle boot",
-]
+# # 10. 加载模型
+# model = NeuralNetwork().to(device)
+# model.load_state_dict(torch.load("model.pth"))
+# classes = [
+#     "T-shirt/top",
+#     "Trouser",
+#     "Pullover",
+#     "Dress",
+#     "Coat",
+#     "Sandal",
+#     "Shirt",
+#     "Sneaker",
+#     "Bag",
+#     "Ankle boot",
+# ]
 
-model.eval()
-with torch.no_grad():
-    X, y = test_data[0][0], test_data[0][1]
-    X = X.to(device)
-    pred = model(X)
-    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+# model.eval()
+# with torch.no_grad():
+#     X, y = test_data[0][0], test_data[0][1]
+#     X = X.to(device)
+#     pred = model(X)
+#     predicted, actual = classes[pred[0].argmax(0)], classes[y]
 
-    print(f"Predicted: '{predicted}', Actual: '{actual}'")
+#     print(f"Predicted: '{predicted}', Actual: '{actual}'")
